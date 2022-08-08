82415563d5b206fd81a36f0bedabfde8147e21de
UlianaOr <Ouliana9@gmail.com>
2022-08-08 16:46:19 +0200

    copyUser

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 let user = {
-    name: "Bob"
+    name: "Bob",
+    age:22,
+    isStudent: true,
+    isMarried: false
 }
 
-let user2 = user
+/*let user2 = user
 user2.name = "Alex"
-console.log (user)
+user2.refregirator = "beer"
+console.log (user)*/
+
+//let copyUser = {}
+   // copyUser.name = user.name
+
+    let copyUser = {... user}
+    copyUser.name = "Alex"
+    console.log (user)
+    console.log (copyUser)
